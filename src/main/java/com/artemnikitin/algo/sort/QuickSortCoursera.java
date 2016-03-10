402ca138c2475b0b06625cca3683b00fb7c38f28
Artem Nikitin <artemnikitin@artemnikitin.com>
2016-03-10 16:13:32 +0100

    Clean for a quick sort example

--- src/main/java/com/artemnikitin/algo/sort/QuickSortCoursera.java
+++ src/main/java/com/artemnikitin/algo/sort/QuickSortCoursera.java
@@ -1,33 +1,20 @@
 package com.artemnikitin.algo.sort;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class QuickSortCoursera {
 
     static int[] data = {20, 8, 7, 0, 15, 35, 11, 0};
-    static long counter = 0;
-    static int first;
-    static int last;
 
     public static void main(String[] args) throws IOException {
-        first = data[0];
-        last = data[data.length - 1];
         System.out.println(Arrays.toString(data));
         quicksort(0, data.length - 1);
         System.out.println(Arrays.toString(data));
-        System.out.println("Num of comparisons : " + counter);
     }
 
     private static void quicksort(int low, int high){
-        //System.out.println("Array length : " + (high - low));
-        //if(high - low > 0) counter += high - low;
         if (high > low) {
-            counter += high - low;
             int i = low, j = high;
             int pivot = low + (high - low) / 2;
             while (i <= j) {
