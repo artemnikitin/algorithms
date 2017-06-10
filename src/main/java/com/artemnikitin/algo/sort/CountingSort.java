6d1c35aae7a85f146db61917c376caba4ba48da9
Artem Nikitin <artemnikitin@artemnikitin.com>
2017-06-10 08:53:36 +0200

    Update counting sort example

--- src/main/java/com/artemnikitin/algo/sort/CountingSort.java
+++ src/main/java/com/artemnikitin/algo/sort/CountingSort.java
@@ -10,20 +10,25 @@ public class CountingSort {
 
     public static void main(String[] args) {
         System.out.println(Arrays.toString(data));
-        countingSort(data, -3, 35);
+        countingSort(data);
         System.out.println(Arrays.toString(data));
     }
 
-    public static void countingSort(int[] data, int min, int max) {
-        // create and fill temp array
+    public static void countingSort(int[] data) {
+        int min = data[0];
+        int max = data[0];
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] < min) min = data[i];
+            if (data[i] > max) max = data[i];
+        }
+
         int[] temp = new int[max - min + 1];
         for (int i = 0; i < data.length; i++) {
             temp[data[i] - min]++;
         }
+
         int index = 0;
-        // in for loop process temp array
         for (int i = min; i <= max; i++) {
-            // if element > 0, then process it
             while (temp[i - min] > 0) {
                 data[index] = i;
                 index++;
